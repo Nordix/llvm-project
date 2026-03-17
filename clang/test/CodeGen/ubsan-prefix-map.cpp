ebeff0b9b7faaf47dee7bc25dbfa3b82b41e709b
Björn Svensson <bjorn.a.svensson@est.tech>
2026-03-17 09:43:15 +0100

    fixup: correcting ubsan-prefix-map.cpp for windows
    Signed-off-by: Björn Svensson <bjorn.a.svensson@est.tech>

--- clang/test/CodeGen/ubsan-prefix-map.cpp
+++ clang/test/CodeGen/ubsan-prefix-map.cpp
@@ -1,5 +1,6 @@
 // RUN: %clang %s -target x86_64-linux-gnu -emit-llvm -S -fsanitize=undefined -o - | FileCheck %s -check-prefix=REGULAR
-// RUN: %clang %s -target x86_64-linux-gnu -emit-llvm -S -fsanitize=undefined -fsanitize-prefix-map=%S/= -o - | FileCheck %s -check-prefix=REMAPPED
+// RUN: %clang %s -target x86_64-linux-gnu -emit-llvm -S -fsanitize=undefined -fsanitize-prefix-map=%/S/= -o - | FileCheck %s -check-prefix=REMAPPED
+// Use %/S which normalizes path separators to forward slashes on all platforms, including Windows.
 
 // REGULAR: @{{.*}} = {{.*}} c"{{.*test(.|\\\\)CodeGen(.|\\\\)ubsan-prefix-map\.cpp}}\00"
 // REMAPPED: @{{.*}} = {{.*}} c"ubsan-prefix-map.cpp\00"
