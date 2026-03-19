2a5827ca6fab84b36ad62ede6a3cf31a54e1331f
Björn Svensson <bjorn.a.svensson@est.tech>
2026-03-19 17:25:05 +0100

    Update  tests

--- clang/test/CodeGen/asan-sanitize-compilation-dir.c
+++ clang/test/CodeGen/asan-sanitize-compilation-dir.c
@@ -2,10 +2,10 @@
 // the ASan module name embedded in instrumented code.
 
 // RUN: mkdir -p %t.dir && cd %t.dir
-// RUN: cp %s rel.c
-// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=address %t.dir/rel.c -o - | FileCheck -check-prefix=CHECK-ABSOLUTE %s
-// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=address -fsanitize-compilation-dir=%t.dir %t.dir/rel.c -o - | FileCheck -check-prefix=CHECK-STRIPPED %s
+// RUN: cp %s test.c
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=address %t.dir/test.c -o - | FileCheck -check-prefix=CHECK-DEFAULT %s
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=address -fsanitize-compilation-dir=%t.dir/ %t.dir/test.c -o - | FileCheck -check-prefix=CHECK-STRIPPED %s
 
-// CHECK-ABSOLUTE: @___asan_gen_module = private constant [{{.*}} x i8] c"{{.+}}rel.c\00"
-// CHECK-STRIPPED: @___asan_gen_module = private constant [{{.*}} x i8] c"rel.c\00"
+// CHECK-DEFAULT: @___asan_gen_module = private constant [{{.*}} x i8] c"{{.+}}test.c\00"
+// CHECK-STRIPPED: @___asan_gen_module = private constant [{{.*}} x i8] c"test.c\00"
 int x;
--- clang/test/CodeGen/ubsan-sanitize-compilation-dir.cpp
+++ clang/test/CodeGen/ubsan-sanitize-compilation-dir.cpp
@@ -7,12 +7,12 @@
 // EmitCheckSourceLocation path.
 
 // RUN: mkdir -p %t.dir && cd %t.dir
-// RUN: cp %s rel.c
-// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=unreachable %t.dir/rel.c -o - | FileCheck -check-prefix=CHECK-ABSOLUTE %s
-// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=unreachable -fsanitize-compilation-dir=%t.dir %t.dir/rel.c -o - | FileCheck -check-prefix=CHECK-STRIPPED %s
+// RUN: cp %s test.c
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=unreachable %t.dir/test.c -o - | FileCheck -check-prefix=CHECK-DEFAULT %s
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=unreachable -fsanitize-compilation-dir=%t.dir/ %t.dir/test.c -o - | FileCheck -check-prefix=CHECK-STRIPPED %s
 
-// CHECK-ABSOLUTE: @{{.*}} = private unnamed_addr constant [{{.*}} x i8] c"{{.+}}rel.c\00"
-// CHECK-STRIPPED: @{{.*}} = private unnamed_addr constant [{{.*}} x i8] c"rel.c\00"
+// CHECK-DEFAULT: @{{.*}} = private unnamed_addr constant [{{.*}} x i8] c"{{.+}}test.c\00"
+// CHECK-STRIPPED: @{{.*}} = private unnamed_addr constant [{{.*}} x i8] c"test.c\00"
 void f(void) {
   __builtin_unreachable();
 }
