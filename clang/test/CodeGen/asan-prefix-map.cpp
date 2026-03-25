6a5de2908eb64334277edd0b234c4b6291fac6e9
Björn Svensson <bjorn.a.svensson@est.tech>
2026-03-25 10:55:25 +0100

    fixup: update tests to use cc1
    Signed-off-by: Björn Svensson <bjorn.a.svensson@est.tech>

--- clang/test/CodeGen/asan-prefix-map.cpp
+++ clang/test/CodeGen/asan-prefix-map.cpp
@@ -1,9 +1,10 @@
-// RUN: %clang_cc1 %s -triple=x86_64-linux-gnu -emit-llvm -fsanitize=address -o - | FileCheck %s -check-prefix=REGULAR
-// RUN: %clang_cc1 %s -triple=x86_64-linux-gnu -emit-llvm -fsanitize=address -fsanitize-prefix-map=%/S/= -o - | FileCheck %s -check-prefix=REMAPPED
-// Use %/S which normalizes path separators to forward slashes on all platforms, including Windows.
+// RUN: mkdir -p %t.dir
+// RUN: cp %s %t.dir/test.c
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=address %t.dir/test.c -o - | FileCheck -check-prefix=REGULAR %s
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=address -fsanitize-prefix-map=%t.dir/= %t.dir/test.c -o - | FileCheck -check-prefix=REMAPPED %s
 
-// REGULAR: @___asan_gen_module = private constant [{{[0-9]+}} x i8] c"{{.*test(.|\\\\)CodeGen(.|\\\\)asan-prefix-map\.cpp}}\00"
-// REMAPPED: @___asan_gen_module = private constant [{{[0-9]+}} x i8] c"asan-prefix-map.cpp\00"
+// REGULAR: @___asan_gen_module = private constant [{{.*}} x i8] c"{{.+}}test.c\00"
+// REMAPPED: @___asan_gen_module = private constant [{{.*}} x i8] c"test.c\00"
 
 int global;
 
--- clang/test/CodeGen/ubsan-prefix-map.cpp
+++ clang/test/CodeGen/ubsan-prefix-map.cpp
@@ -1,9 +1,14 @@
-// RUN: %clang %s -target x86_64-linux-gnu -emit-llvm -S -fsanitize=undefined -o - | FileCheck %s -check-prefix=REGULAR
-// RUN: %clang %s -target x86_64-linux-gnu -emit-llvm -S -fsanitize=undefined -fsanitize-prefix-map=%/S/= -o - | FileCheck %s -check-prefix=REMAPPED
-// Use %/S which normalizes path separators to forward slashes on all platforms, including Windows.
+// We use -fsanitize=signed-integer-overflow (included in -fsanitize=undefined)
+// rather than -fsanitize=undefined because the latter is a driver-level umbrella
+// flag not accepted by cc1.
 
-// REGULAR: @{{.*}} = {{.*}} c"{{.*test(.|\\\\)CodeGen(.|\\\\)ubsan-prefix-map\.cpp}}\00"
-// REMAPPED: @{{.*}} = {{.*}} c"ubsan-prefix-map.cpp\00"
+// RUN: mkdir -p %t.dir
+// RUN: cp %s %t.dir/test.cpp
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=signed-integer-overflow %t.dir/test.cpp -o - | FileCheck -check-prefix=REGULAR %s
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -emit-llvm -fsanitize=signed-integer-overflow -fsanitize-prefix-map=%t.dir/= %t.dir/test.cpp -o - | FileCheck -check-prefix=REMAPPED %s
+
+// REGULAR: @{{.*}} = private unnamed_addr constant [{{.*}} x i8] c"{{.+}}test.cpp\00"
+// REMAPPED: @{{.*}} = private unnamed_addr constant [{{.*}} x i8] c"test.cpp\00"
 
 int f(int x, int y) {
   return x / y;
